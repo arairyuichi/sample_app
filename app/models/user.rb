0ff2eff0d2e17912443d174dee0173496513c215
Green One <fortyremark@yahoo.co.jp>
2017-05-18 21:40:03 +0000

    Finish user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,8 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
+    # Micropost.where("user_id = ?", id)
   end
   
   def following?(other_user)
